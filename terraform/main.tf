fd6c34c206d45775dc1534281ff3976143a291a1
Jonathan JEHANNO <jehanno.jonathan@gmail.com>
2026-05-22 13:54:21 +0200

    feat: infra Azure Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,116 @@
+locals {
+  vm_name              = "${var.rg_name}-vm"
+  vnet_name            = "${var.rg_name}-vnet"
+  subnet_name          = "${var.rg_name}-subnet"
+  nsg_name             = "${var.rg_name}-nsg"
+  public_ip_name       = "${var.rg_name}-pip"
+  nic_name             = "${var.rg_name}-nic"
+  vm_admin_username    = "azureuser"
+  storage_account_base = replace(replace(replace(lower(var.rg_name), "-", ""), "_", ""), " ", "")
+  storage_account_name = substr("${local.storage_account_base}sa", 0, 24)
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = local.vnet_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = local.subnet_name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "this" {
+  name                = local.nsg_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_public_ip" "this" {
+  name                = local.public_ip_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "this" {
+  name                = local.nic_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.this.id
+  }
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name                            = local.vm_name
+  location                        = azurerm_resource_group.this.location
+  resource_group_name             = azurerm_resource_group.this.name
+  size                            = var.vm_size
+  admin_username                  = local.vm_admin_username
+  network_interface_ids           = [azurerm_network_interface.this.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = local.vm_admin_username
+    public_key = tls_private_key.ssh.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
